bca677cb876ea5c5ad23463eef404983ba39422a
Minho Menezes <minhotmog@gmail.com>
2020-03-27 23:30:09 -0300

    Minor changes in the scripts

--- src/scripts/optimization/newton_SQP.jl
+++ src/scripts/optimization/newton_SQP.jl
@@ -12,33 +12,30 @@ meshgrid(X,Y) = (first.(collect(Iterators.product(X, Y))), last.(collect(Iterato
 # ===================
 
 # ==== Functions ====
-function SQP_solver(G, H, ∇C, C_LU, X_LU, x0;S0=Nothing, ϵ=1e-6, T=1e3, verbose=false)
-# (X,λ,ν) = SQP_SOLVER(G, H, C_LU, X_LU, x0;S0=Nothing, ϵ=1e-6, T=1e3, verbose=false) 
+function SQP_solver(∇∇L, ∇F, A, x0; S0=Nothing, ϵ=1e-6, T=1e3, verbose=false)
+# (X,λ) = SQP_SOLVER(∇∇L, ∇F, A, x0; S0=Nothing, ϵ=1e-6, T=1e3, verbose=false) 
 #	given a nonlinear objective function F: R^n -> R, nonlinear contraints C: R^n -> R^m, 
 #	and initial point x0 ∈ R^n, solves the optimization problem
-#				min_(x)		  F(x) 
-#				s.t.	C_L ≤ C(x) ≤ C_U
-#						x_L ≤   x  ≤ x_U
+#				min_(x)	F(x) 
+#				s.t.	C(x) = 0
 #	by using a Sequential Quadratic Programming approach.
 #	The current solution is optimized by minimizing a QP subproblem by considering a 
 #	quadratic approximation of the Lagrangian function and a linear approximation of
 #	the contraints. The equivalent optimization is:
-#				min_(p)	L(p) ≈ G'p + 1/2 p'Hp
-#				s.t.	C_L - c(x) ≤ ∇C*p ≤ C_U - c(x)
-#						   x_L - x ≤  p  ≤ x_U - x
+#				min_(p)	L(p) ≈ (∇F_k)'p + 1/2 p'(∇²_x L_k)p
+#				s.t.	A_k p + c_k = 0
 #	Each step updates the solution as
-#		| x_(k+1) |   | x_k |	  |   p  |   (x_0 = x0).
-#		| λ_(k+1) |	= | λ_k | + η | λ'-λ |
-#		| ν_(k+1) |	  | ν_k |     | ν'-ν |
-#		| s_(k+1) |	  | s_k |     | s'-s |
-#	with p,ν' ∈ R^n and λ',s' ∈ R^m, being the solutions of the equivalent
-#	QP Karush-Kuhn-Tucker (KKT) system.
+#		| x_(k+1) | = | x_k + p |	(x_0 = x0).
+#		| λ_(k+1) |	  |	λ_(k+1) |
+#	with p ∈ R^n and λ_(k+1) ∈ R^m, being the solutions of the equivalent
+#	Karush-Kuhn-Tucker (KKT) system:
+#		| ∇²_x L_k   -A_k' | |   p     | = - | ∇F_k |  .
+#		|    A_k       0   | | λ_(k+1) |     |  c_k |
 #
-#	The outputs are X = [x_0, ..., x_T], and multipliers λ = [λ_0, ..., λ_T] 
-#	and ν = [ν_0, ..., ν_T]. 
+#	The outputs are X = [x_0, ..., x_T], and multipliers λ = [λ_0, ..., λ_T]. 
 	return Nothing
-
 end	
+
 # ===================
 
 # ==== Variables ====
--- src/scripts/optimization/newton_eqcon.jl
+++ src/scripts/optimization/newton_eqcon.jl
@@ -43,12 +43,14 @@ function newtonMethod_KKT(F, c, x0; η=1, ∇F=Nothing, ∇∇F=Nothing, ∇c=No
 	end
 	for k ∈ 1:T
 		# Solves the Karush-Kuhn-Tucker (KKT) system
-		KKT   = [∇∇L(X[:,end], λ[:,end])  ∇c(X[:,end])'
-			          ∇c(X[:,end])          0I(m)     ];
-		KKT_s = KKT^(-1) *[∇F(X[:,end]); c(X[:,end])]
+		KKT   = [∇∇L(X[:,end], λ[:,end]) -∇c(X[:,end])'
+			          ∇c(X[:,end])             0      ];
+		KKT = Matrix(KKT)
+
+		KKT_s = KKT^(-1) * -[∇F(X[:,end]); c(X[:,end])]
 
 		# Calculate the optimization step
-		p_k = -KKT_s[1:n];
+		p_k = KKT_s[1:n];
 
 		# Updates the parameter and lagrange multipliers values
 		x_k = X[:,end] + η*p_k
@@ -64,8 +66,8 @@ function newtonMethod_KKT(F, c, x0; η=1, ∇F=Nothing, ∇∇F=Nothing, ∇c=No
 		end
 
 		# Prints the optimization step
-		if(verbose); println("$(@sprintf("%d",k)) | $(x_k) | $(F(x_k)) | $(p_size)")
-		end
+		# if(verbose); println("$(@sprintf("%d",k)) | $(x_k) | $(F(x_k)) | $(p_size)")
+		# end
 
 		# Increases the parameters and lagrange multipliers vector
 		X = [X x_k]; λ = [λ λ_k]
@@ -78,32 +80,35 @@ end
 # ===================
 
 # ==== Variables ====
-F(x)   = x'x
-∇F(x)  = 2x
-∇∇F(x) = 2I(size(x,1))
+F(x)  	= x[1]^2 + x[2]^2 + log(x[1]*x[2])
+∇F(x)	= [ 2x[1]+x[1]^-1 
+			2x[2]+x[2]^-1]
+∇∇F(x)	= [ 2-x[1]^-2  	 0	   
+			  0		  2-x[2]^-2]
 
-c(x)   = [1 0.2; 1 1]'x .- 2
-∇c(x)  = [1 0.2; 1 1]'
-∇∇c(x) = 0
+c(x)  = x[1]*x[2]-1
+∇c(x) = [x[1]
+		 x[2]]'
+∇∇c(x) = I(2)
 
-x0 = [-1.5; 1.5]
+x0 = [0.5; 1]
 
 # ===================
 
 # ==== Script ====
-(X,λ) = newtonMethod_KKT(F, c, x0, η=0.2, verbose=true,
+(X,λ) = newtonMethod_KKT(F, c, x0, η=1, verbose=true,
 							∇F=∇F, ∇∇F=∇∇F, ∇c=∇c, ∇∇c=∇∇c)
 
 # 2. Plot the optimization steps
 # Generates the error surface
-(xx1, xx2) = meshgrid(-3:0.1:5, -3:0.1:5)
+(xx1, xx2) = meshgrid(0.01:0.1:5, 0.01:0.1:5)
 xx = [[x1,x2] for (x1,x2) in zip(xx1[:], xx2[:])]
 
 Fx = reshape(F.(xx), size(xx1))
 cx = reshape(c.(xx), size(xx1))
 FA = ([map(x->x[i], cx) for i in 1:length(cx[1])])
 
-contour(xx1, xx2, Fx,
+p = contour(xx1, xx2, Fx,
 		xlim=(min(xx1...), max(xx1...)), 
     	ylim=(min(xx2...), max(xx2...)),
     	size=(16,10).*60, dpi=200, grid=false)
@@ -111,6 +116,8 @@ contour(xx1, xx2, Fx,
 for i in 1:length(FA)
 	contour!(xx1, xx2, FA[i], levels=0, c=RGBA(0.2,0.8,0.2,1))
 end
+
+display(p)
 
 plot!(X[1,:], X[2,:], l=(1, :blue), m=(:star5, :white, 6, stroke(0)))
 
